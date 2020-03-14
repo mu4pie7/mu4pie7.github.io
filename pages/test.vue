3a60d345d9867bded453387038870bb23c210a3d
mu4pie7 <mu4pie7@gmail.com>
2020-03-14 11:59:38 +0900

    i pug & stylus

---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+.container
+  h1.red Hello {{ name }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'みゅー'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.container
+  background-color red
+  height 100vh
+  display flex
+  align-items center
+  justify-content center
+.red
+  color #fff
+</style>
