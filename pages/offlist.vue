74cd2ffc094f0737c05f5c8dbe03ad58dca10e1c
mu4pie7 <mu4pie7@gmail.com>
2020-03-14 16:22:01 +0900

    import sample

--- pages/offlist.vue
+++ pages/offlist.vue
@@ -1,4 +1,72 @@
 <template lang="pug">
 section
   h2 配布可能曲リスト
+    el-table(:data="tableData" style="width: 100%")
+      el-table-column(type="expand")
+        template(slot-scope="props")
+          p State: {{ props.row.state }}
+          p City: {{ props.row.city }}
+          p Address: {{ props.row.address }}
+          p Zip: {{ props.row.zip }}
+      el-table-column(label="Date" prop="date")
+      el-table-column(label="Name" prop="name")
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }, {
+          date: '2016-05-02',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }, {
+          date: '2016-05-04',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }, {
+          date: '2016-05-01',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }, {
+          date: '2016-05-08',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }, {
+          date: '2016-05-06',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }, {
+          date: '2016-05-07',
+          name: 'Tom',
+          state: 'California',
+          city: 'Los Angeles',
+          address: 'No. 189, Grove St, Los Angeles',
+          zip: 'CA 90036'
+        }]
+      }
+    }
+  }
+</script>
